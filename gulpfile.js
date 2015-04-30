debd69e1738cb99c9a4c5f974bc0460d608e073c
Einari Naukkarinen <einari.naukkarinen@outlook.com>
2015-04-30 19:39:08 +0300

    initial paske

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var gulp = require('gulp');
+var browserify = require('browserify');
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var gutil = require('gulp-util');
+
+
+gulp.task('default', function() {
+  console.log('gulp default task!');
+});
+
+gulp.task('javascript', function () {
+  // set up the browserify instance on a task basis
+  var b = browserify({
+    entries: './entry.js',
+    debug: true
+  });
+
+  return b.bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        .pipe(uglify())
+        .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/js/'));
+});
