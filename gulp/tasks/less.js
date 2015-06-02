0979c7b592bc9a061b7bc19ae2da9cd84faa034d
einarinau <einari.naukkarinen@basware.com>
2015-06-02 15:43:10 +0300

    error handling to less compiling

--- gulp/tasks/less.js
+++ gulp/tasks/less.js
@@ -1,20 +1,24 @@
 'use strict';
 
 var gulp 		      = require('gulp');
+var gutil         = require('gulp-util');
 var less 		      = require('gulp-less');
 var sourcemaps    = require('gulp-sourcemaps');
 var autoprefixer  = require('gulp-autoprefixer');
 var config 		    = require('../config.js')();
 var concat        = require('gulp-concat');
-
+var plumber       = require('gulp-plumber');
 
 gulp.task('less', function () {
+  
   return gulp
     .src(config.less)
     .pipe(sourcemaps.init())
+    .pipe(plumber())
     .pipe(less())
     .pipe(autoprefixer({ map: true })) 
     .pipe(concat('styles.css'))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(config.dist));
+
 });
