7d47efab8b94c0bd0da0cdcb8f8aa620ade2b2ac
einarinau <einari.naukkarinen@basware.com>
2015-06-02 16:23:39 +0300

    html watch

--- gulp/config.js
+++ gulp/config.js
@@ -3,14 +3,19 @@ module.exports = function() {
   return {
     // paths
     allts: '.src/**/*.ts',
+    allhtml: '.src/**/*.html',
     less: 'src/styles/**/*.less',
     dist: './dist/',
+    
+    // vendor bundle
     vendorscripts: [
       './bower_components/jquery/dist/jquery.js',
       './bower_components/lodash/lodash.js',
       './bower_components/angular/./angular.js',
       './bower_components/bootstrap/dist/js/bootstrap.js'
     ],
+    
+    // vendor css bundle
     vendorstyles: [
       './bower_components/bootstrap/dist/css/bootstrap.css'
     ]  
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,11 +1,18 @@
 
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var config = require('../config.js')();
 
-gulp.task('watch', ['js-watch', 'less-watch']);
+gulp.task('watch', ['js-watch', 'less-watch', 'html-watch'], function() { 
+    gutil.log(gutil.colors.green('WATCHING EVERYTHING!'));
+});
 
 gulp.task("js-watch", function() {
     gulp.watch(config.allts, ['app']);
+});
+
+gulp.task("html-watch", function() {
+    gulp.watch(config.allhtml, ['views']);
 });
 
 gulp.task('less-watch', function () {
