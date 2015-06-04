2d17c5e323b754ebaa29909b04ccf345fc41a2fd
Einari Naukkarinen <einari.naukkarinen@outlook.com>
2015-06-04 22:26:15 +0300

    basic nodemon event handling

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 var config = require('../config.js')();
+var gutil = require('gulp-util');
 
 gulp.task('serve', function(){
 	console.log(config.defaultport);
@@ -21,7 +22,20 @@ gulp.task('serve', function(){
 		
 	};
 	
-	return nodemon(nodeOptions);
+	return nodemon(nodeOptions)
+	.on('restart', function(ev){
+		gutil.log(gutil.colors.green('*** nodemon restarted...'));
+		gutil.log(gutil.colors.green('files changed on restart:\n', ev));
+	})
+	.on('start', function(){
+		gutil.log(gutil.colors.cyan('*** nodemon started...'));
+	})
+	.on('crash', function(){
+		gutil.log(gutil.colors.red('*** NODEMON CRASHED: script crashed for some reason.'));
+	})
+	.on('exit', function(){
+		gutil.log(gutil.colors.magenta('*** nodemon exited cleanly...'));
+	});
 	
 	
 });
