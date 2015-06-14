c4b32fcdf3027d08dd24b4aea9e9c070849b6d94
einarinau <einari.naukkarinen@basware.com>
2015-06-15 01:56:19 +0300

    shameful 'this' fix for PlayersController

--- src/client/features/players/Player.ts
+++ src/client/features/players/Player.ts
@@ -3,8 +3,15 @@
 
 export default class Player {
 	
+	id: number;
 	name: string;
 	age: number;
 	position: string;
 	
+	constructor(i: number, n: string, a: number, p: string) {
+		this.id = i;
+		this.name = n;
+		this.age = a;
+		this.position = p;
+	}
 }
--- src/client/features/players/PlayersController.ts
+++ src/client/features/players/PlayersController.ts
@@ -8,45 +8,44 @@ import PlayerModalService from "./PlayerModalService";
 
 export default class PlayersCtrl {
 	
-	players: Player[];
+	public players: Player[];
 	
 	static $inject = ['PlayerModalService'];
-	
-	constructor(private ModalService : PlayerModalService) {
 
-		this.players = [
-			{id: 1, name: "Karl Anthony Towns", age: 18, position: "C"},
-			{id: 2, name: "Jahlil Okafor", age: 19, position: "C"},
-			{id: 3, name: "D'Angelo Russell", age: 19, position: "G"},
-			{id: 4, name: "Emmanuel Mudiay", age: 20, position: "G"}
-		];
+	constructor(private ModalService : PlayerModalService) {
+		this.players = new Array<Player>();
+		this.players.push(new Player(1, "Karl Anthony Towns", 18, "C"));
+		this.players.push(new Player(2, "Jahlil Okafor", 19, "C"));
+		this.players.push(new Player(3, "D'Angelo Russell", 19, "G"));
+		this.players.push(new Player(4, "Emmanuel Mudiay", 19, "G"));
 	}
 	
 
 	
 	public create () {
-		
+		var temp = this.players;
 		 this.ModalService.showModal({action: "create", player: {id: this.players.length+1, name: "", age: 18, position: ""}}).then(function (created) { 
-			this.players.push(created);
+			temp.push(created);
 		});
-		
+		this.players = temp;
 	};
 	
 	public edit (player) {
-		
+		var temp = this.players;
 		 this.ModalService.showModal({action: "edit", player: angular.extend({}, player)}).then(function (edited) { 
-			var index = _.indexOf(this.players, _.find(this.players, {id: player.id}));
-			this.players.splice(index, 1, edited);
+			var index = _.indexOf(temp, _.find(temp, {id: player.id}));
+			temp.splice(index, 1, edited);
 		});
+		this.players = temp;
 		
 	};
 	
 	public delete (player) {
-		
+		var temp = this.players;
         this.ModalService.showModal({action: "delete", player: player}).then(function (result) { 
-			_.remove(this.players, function(obj) { return obj.id === player.id; }); 
+			_.remove(temp, function(obj) { return obj.id === player.id; }); 
 		});
-						
+		this.players = temp;						
 	};
 }
 
