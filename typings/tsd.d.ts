ebb678f4a70d7b691bf2c7393bcc428ce77f5f75
Einari Naukkarinen <einari.naukkarinen@basware.com>
2015-09-22 10:13:56 +0300

    updated dependencies

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,14 +1,10 @@
-
-/// <reference path="lodash/lodash.d.ts" />
-
-/// <reference path="jquery/jquery.d.ts" />
-
-/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="angular-ui-bootstrap/angular-ui-bootstrap.d.ts" />
 /// <reference path="angularjs/angular-animate.d.ts" />
 /// <reference path="angularjs/angular-cookies.d.ts" />
 /// <reference path="angularjs/angular-mocks.d.ts" />
-/// <reference path="angularjs/angular-resource.d.ts" />
 /// <reference path="angularjs/angular-route.d.ts" />
 /// <reference path="angularjs/angular-sanitize.d.ts" />
-/// <reference path="angular-ui-bootstrap/angular-ui-bootstrap.d.ts" />
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="lodash/lodash.d.ts" />
 /// <reference path="toastr/toastr.d.ts" />
