2af7a603c2e4f915123fd94802576c43ec387bae
Einari Naukkarinen <einari.naukkarinen@basware.com>
2015-11-05 21:25:56 +0200

    deleted demo app

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
 /// <reference path="../typings/tsd.d.ts" />
 
 import "./templates";
-import "./features/common/navbar/NavBarModule";
-import "./features/players/PlayersModule";
 
 angular.module("app", [
   "app.templates",
-  "NavBarModule",
-  "PlayersModule",
   "ui.router",
   "ui.bootstrap"])
  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
@@ -17,22 +13,8 @@ angular.module("app", [
 			name: "welcome",
 			url: "/welcome",
   			templateUrl: "./features/welcome/Welcome.html"
-		})
-		.state("players", {
-			name: "Players",
-			url: "/Players",
-        	templateUrl: "./features/players/PlayersGrid.html",
-			controller: "PlayersController",
-			controllerAs: "pl"
-		})
-		.state("Player", {
-			name: "Player",
-			url: "/Player",
-        	templateUrl: "./features/players/PlayerDetails.html",
-			controller: "PlayerController",
-			controllerAs: "pld",
-			params: {player: null}
 		});
+		
 }]);
 
 
