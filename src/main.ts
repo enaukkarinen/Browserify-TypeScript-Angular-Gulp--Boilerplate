1b3818be109edf998de26d2891200fb92e3187a1
Einari Naukkarinen <einari.naukkarinen@outlook.com>
2015-11-19 12:22:56 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -35,15 +35,3 @@ angular.module("app", [
 		});
 }]);
 
-
-// Display a warning toast, with no title
-//toastr.warning('My name is Inigo Montoya. You killed my father, prepare to die!')
-
-// Display a success toast, with a title
-//toastr.success('Have fun storming the castle!', 'Miracle Max Says')
-
-// Display an error toast, with a title
-//toastr.error('I do not think that word means what you think it means.', 'Inconceivable!')
-
-// Override global options
-//toastr.success('We do have the Kapua suite available.', 'Turtle Bay Resort', {timeOut: 5000})
